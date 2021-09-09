365cefdf4c91aef6b1d5bfb7574e2c1c6c26c3a7
kakimli <lmy.formal@gmail.com>
2021-09-09 18:50:09 +0800

    db connection

--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     APP_SECRET: '308dacc1d9b8f189ffcc3ee69ecda8d3',
-    MONGODB_URL: 'mongodb://localhost:27017/sdpt_housing'
+    MONGODB_URL: 'mongodb://localhost:27017/sdpt_housin'
 };
 exports.default = config;
 //# sourceMappingURL=config.js.map
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   APP_SECRET: '308dacc1d9b8f189ffcc3ee69ecda8d3',
-  MONGODB_URL: 'mongodb://localhost:27017/sdpt_housing'
+  MONGODB_URL: 'mongodb://sdptHousing:sdpt0828@127.0.0.1:27017/sdpt-housing'
 }
 
 export default config;
