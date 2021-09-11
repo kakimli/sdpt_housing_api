5a2e54afb24f3d5cdb2f6d1acb7a9b872f1bcc45
kakimli <lmy.formal@gmail.com>
2021-09-11 16:44:48 +0800

    modify dist

--- dist/config.js
+++ dist/config.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     APP_SECRET: '308dacc1d9b8f189ffcc3ee69ecda8d3',
-    MONGODB_URL: 'mongodb://localhost:27017/sdpt_housing',
-    maxPostCount: 3
+    MONGODB_URL: 'mongodb://sdptHousing:sdpt0828@127.0.0.1:27017/sdpt-housing',
+    maxPostCount: 30
 };
 exports.default = config;
 //# sourceMappingURL=config.js.map
