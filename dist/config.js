8e60ea5dc7dae07889cf1f6b4c5c389054090299
kakimli <lmy.formal@gmail.com>
2021-09-18 21:34:33 +0800

    hide secret

--- dist/config.js
+++ dist/config.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
-    APP_SECRET: '308dacc1d9b8f189ffcc3ee69ecda8d3',
-    MONGODB_URL: 'mongodb://sdptHousing:sdpt0828@127.0.0.1:27017/sdpt-housing',
+    APP_SECRET: '',
+    MONGODB_URL: '',
     maxPostCount: 30
 };
 exports.default = config;
