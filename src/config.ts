0d9d158f7cafc6214939b2eb688ca82c71efc3e0
kakimli <lmy.formal@gmail.com>
2021-09-18 21:14:26 +0800

    public display - delete secret infomation

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  APP_SECRET: 'e270be459a851aa793f70f052656e433',
-  MONGODB_URL: 'mongodb://sdptHousing:sdpt0828@127.0.0.1:27017/sdpt-housing',
+  APP_SECRET: 'secret',
+  MONGODB_URL: 'secret',
   // MONGODB_URL: 'mongodb://localhost:27017/sdpt_housing',
   maxPostCount: 30
 }
